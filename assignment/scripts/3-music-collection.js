558540a328752eacfdec27fdd6c8e06b212f307d
IanMckL <96411486+IanMckL@users.noreply.github.com>
2022-03-28 15:43:59 -0500

    Update 3-music-collection.js

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,10 +10,17 @@ function addToCollection (title, artist, yearPublished){
 }
 
     collection.push(toAdd);
-    return collection;
+    return collection[0];
 }
 
 console.log(addToCollection('Bobby Sounds', 'Bob', '1968'));
+addToCollection('Electra Complex', 'Marin', '2011');
+addToCollection('Darkplanet', 'Davey Bow', '2015');
+addToCollection(`Grum's Greatest Hits`, 'Grum' , '2000');
+addToCollection('DJ Dumbly', 'Party Mix 10' , '2023');
+addToCollection(`Rocky Horror Painting Exhibition`, 'Live in 2002', '2002')
+addToCollection('Bobby Sounds 2','Bob','1970')
+
 //Parameter 2
 function showCollection(array){
     console.log(array, 'The length of the collection is' + array.length + 'entries');
@@ -36,5 +43,5 @@ function findByArtist(artist){
     return tempArray;
 }
 
-console.log(findByArtist('Bob'));
-console.log(findByArtist('Marina'));
+console.log(`Should be array with objects ==>`, findByArtist('Bob'));
+console.log(`Should be empty ==>`, findByArtist('Marina'));
